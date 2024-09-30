38a6666cf1c5069f25cbeaafb4c35440c3b6e2eb
kimtth <iyai@naver.com>
2024-09-30 17:33:43 +0900

    _
    app: exo, crawl4ai, llama stack, langgraph doc, argus, copilot next phase, voiceRAG, o1-engineer, use keyless auth with aoai, sk eval pipeline (micronaire), ai template collection, baseline architecture
    llm: molmo, alphachip
    ref: umar jamil, rag and beyond, openai devday 2024
    > tidying up old files and folders.

--- code/deeplearning.ai/quality-and-safety-for-llm-applications/helpers.py
+++
@@ -1,4 +0,0 @@
-import sys
-
-sys.path.append('./extras')
-from langkit_bounty_helpers import *
--- code/llama-index-es-handson/settings.py
+++
@@ -1,8 +0,0 @@
-from dotenv import dotenv_values
-
-
-class Settings:
-    @staticmethod
-    def config() -> dict[str, str | None]:
-        config_env = dotenv_values(".env")
-        return config_env
